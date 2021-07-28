0e6e67dc4d45c07c280f4805fe40e6d9de761f2c
Indra Ardiansah <indra.ardiansah@itechshark.com>
2021-07-28 23:25:47 +0700

    Add vue router

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import { createVuePlugin } from 'vite-plugin-vue2'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    createVuePlugin(),
+  ],
+  server: {
+    port: 8080,
+  },
+  resolve: {
+    alias: [
+      {
+        find: '@',
+        replacement: path.resolve(__dirname, 'src')
+      }
+    ]
+  },
+})
